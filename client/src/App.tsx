b0047b57c5370587543e1f55a43ece3767ceaf4f
evan-howie <etshowie@gmail.com>
2024-01-27 23:09:09 -0500

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './screens/Home';
 import Profile from './screens/Profile';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            display: 'none',
+          },
+        }}>
+        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
     </NavigationContainer>
